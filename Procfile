b636471e143c458fda1c73bff5eebb597e4639a8
Ben Gosney <bengosney@googlemail.com>
2022-04-30 19:15:28 +0100

    Inital site framework

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn stl.wsgi --log-file -
---
+++ stl/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-5y%vys$7ftzwnthn@ud8vl&=w#9d)b1%i8r(6d2b35!jt+uf^3'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
