090ef03a849bcf20d54c09219108025554d831c4
Ben Gosney <bengosney@googlemail.com>
2022-05-19 08:55:16 +0100

    Fix error when there is no valid banner

--- banners/templatetags/banner_tags.py
+++ banners/templatetags/banner_tags.py
@@ -9,6 +9,8 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_banner(context):
-    banner = Banner.objects.all()[0]
-
-    return banner.image
+    try:
+        banner = Banner.objects.all()[0]
+        return banner.image
+    except IndexError:
+        return None
