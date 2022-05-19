f0dec9bbff98c281d5b243bd149f993b2cca1788
Ben Gosney <bengosney@googlemail.com>
2022-05-19 21:25:09 +0100

    First test for site messages

--- site_messages/tests.py
+++ site_messages/tests.py
@@ -1,1 +1,14 @@
-# Create your tests here.
+# Django
+from django.test import TestCase
+
+# Third Party
+from model_bakery import baker
+
+# Locals
+from .models import Message
+
+
+class TestSiteMessages(TestCase):
+    def test_key(self):
+        msg = baker.make(Message)
+        self.assertEqual(msg.session_key, f"message-{msg.slug}-dismissed")
