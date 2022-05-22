b52c998237555f30900761efaf3303583e10ed99
Ben Gosney <bengosney@googlemail.com>
2022-05-22 21:13:38 +0100

    Register rustface

--- pages/apps.py
+++ pages/apps.py
@@ -1,7 +1,15 @@
 # Django
 from django.apps import AppConfig
 
+# Third Party
+import rustface.willow
+from willow.registry import registry
+
 
 class PagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pages"
+
+    def ready(self) -> None:
+        registry.register_plugin(rustface.willow)
+        return super().ready()
