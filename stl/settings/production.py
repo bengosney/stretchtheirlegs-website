6ac6c5dcbfd248d1fff385f09f3cdd8df139da09
Ben Gosney <bengosney@googlemail.com>
2022-05-22 09:09:37 +0100

    Added health check

--- stl/settings/production.py
+++ stl/settings/production.py
@@ -3,6 +3,14 @@ from .base import *  # noqa
 
 DEBUG = False
 
+INSTALLED_APPS += [  # noqa
+    "health_check.contrib.migrations",
+    "health_check.contrib.psutil",
+    "health_check.contrib.s3boto3_storage",
+    "health_check.contrib.redis",
+    "health_check.db",
+]
+
 try:
     # Locals
     from .local import *  # noqa
