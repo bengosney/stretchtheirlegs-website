4d71b298771b33d2c201236d3b0cbf5402171d7b
Ben Gosney <bengosney@googlemail.com>
2023-05-29 19:00:14 +0100

    Fix banner images for articles

--- banners/templatetags/banner_tags.py
+++ banners/templatetags/banner_tags.py
@@ -10,6 +10,10 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_banner(context):
     try:
-        return context.get("page", None).banner_image
+        if banner_image := context.get("page", None).banner_image:
+            return banner_image
     except AttributeError:
-        return banner.image if (banner := Banner.get_current_image()) else None
+        pass
+
+    banner = Banner.get_current_image()
+    return banner.image if banner else None
