bbf5302f13673b603f84836779e2317d9b82f0a6
Ben Gosney <bengosney@googlemail.com>
2023-06-05 13:58:36 +0100

    Server SCSS files in dev

--- stl/settings/dev.py
+++ stl/settings/dev.py
@@ -1,5 +1,9 @@
+# Standard Library
+import os
+
 # Locals
 from .base import *  # noqa
+from .base import INSTALLED_APPS, MIDDLEWARE, PROJECT_DIR, STATICFILES_DIRS
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -13,12 +17,12 @@ ALLOWED_HOSTS = ["*"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-INSTALLED_APPS += [  # noqa
+INSTALLED_APPS += [
     "debug_toolbar",
     "debugtools",
 ]
 
-MIDDLEWARE += [  # noqa
+MIDDLEWARE += [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
@@ -43,6 +47,13 @@ CSP_IMG_SRC = None
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+]
+
+SCSS_SOURCEMAP_URL = "/scss/"
+SCSS_SOURCEMAP_ROOT = os.path.join(PROJECT_DIR, "..", "scss")
+
+STATICFILES_DIRS += [
+    os.path.join(PROJECT_DIR, "..", "scss"),
 ]
 
 try:
