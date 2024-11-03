65e328c86e3bfc7ec1deba3b81ec3afc9a3c5ebd
Ben Gosney <bengosney@googlemail.com>
2024-11-03 13:36:51 +0000

    test(forms): test the shorten_lenght function

---
+++ pages/tests/test_shorten_label.py
@@ -0,0 +1,28 @@
+import pytest
+
+from pages.models import shorten_label
+
+
+@pytest.fixture(autouse=True)
+def use_max_form_title_length(settings):
+    settings.MAX_FORM_TITLE_LENGTH = 11
+
+
+def test_shorten_label_shorter_than_max():
+    label = "Short"
+    assert shorten_label(label) == "Short"
+
+
+def test_shorten_label_exactly_max():
+    label = "ExactLength"
+    assert shorten_label(label) == "ExactLength"
+
+
+def test_shorten_label_longer_than_max():
+    label = "This is a very long label"
+    assert shorten_label(label) == "This is..."
+
+
+def test_shorten_label_empty():
+    label = ""
+    assert shorten_label(label) == ""
